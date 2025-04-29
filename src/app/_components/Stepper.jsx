c58d518bd4877271712a5ad6b198dfeb8fdf1980
tsetse0725 <tse9406@gmail.com>
2025-04-29 11:37:37 +0800

    new

---
+++ src/app/_components/Stepper.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+const Comp = [A, B, C];
+const Stepper = comp[ind];
+const [index, setIndex] = useState(0);
+setIndex((prev) => {
+  prev + 1;
+});
+export default Stepper;
